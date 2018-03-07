cfc739af4bfcef9e671c05124819e835b96c622e
mallocc <gh@alexeverett.uk>
2018-03-07 00:42:59 +0000

    upadte

--- main_program/main_program.ino
+++ main_program/main_program.ino
@@ -9,64 +9,47 @@ char myChar;
 
 int load = 0;
 const int ledPin =  13;
-const int button_pin =  6;
-int button_pressed = 0;
-int button_pressed_old = 0;
-
+const int button_pin =  9;
 int power_state = POWER_OFF;
-int power_time_ticks = 10;
+int power_time_ticks = 3;
 int power_time_counter = 0;
-
-int bt_discover_button_counter = 0;
-int bt_discover_button_ticks = 5;
 
 void setup() {
   pinMode(button_pin, INPUT);
-    pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   // Open serial communications:
   Serial.begin(9600);
   // The HC-06 defaults to 9600 according to the datasheet.
   BTSerial.begin(9600);
   Serial.print("BT serial started...");
+  delay(1000);
 }
 
 void loop() {
-    digitalWrite(ledPin, HIGH);
-    
-  while (BTSerial.available()) {
-    myChar = BTSerial.read();
-    Serial.print(myChar);
-  }
 
-  while (Serial.available()) {
-    myChar = Serial.read();
-    Serial.print(myChar); //echo
-    BTSerial.print(myChar);
-  }
-
-  load++;
-  BTSerial.print(String(load));
-
-  button_pressed = digitalRead(button_pin);
-
-  if (button_pressed)
+int state = digitalRead(button_pin);
+  if (state)
   {
-    bt_discover_button_counter++;
-    if (!button_pressed_old) // inital press
-    {
-      power_state = POWER_ON;
-      power_time_counter = 0;
-      //bt_discover_button_counter = 0;
-    }
-  }        
+    Serial.println("button pressed");
+    power_state = POWER_ON;
+    power_time_counter = 0;
+  }
 
   if (power_state == POWER_ON)
+  {
+    Serial.println("tick power on");
+    //    digitalWrite(ledPin, HIGH);
+    //    delay(100);
     ++power_time_counter;
+    BTSerial.println(String(load++));
+  }
+  else
+  {
+       Serial.println("tick power off");
+  }
 
   if (power_time_counter > power_time_ticks)
     power_state = POWER_OFF;
 
-  button_pressed_old = button_pressed;
-  
-  LowPower.powerDown(power_state ? SLEEP_250MS : SLEEP_2S, ADC_OFF, BOD_OFF);
+  LowPower.powerDown(power_state ? SLEEP_250MS : SLEEP_1S, ADC_OFF, BOD_OFF);
 }
